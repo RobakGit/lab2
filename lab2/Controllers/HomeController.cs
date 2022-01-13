9dc302f65de7e23031ccd685fc5dc0b11ef5ab12
Miłosz Robak <milosz@wandlee.com>
2022-01-13 20:48:22 +0100

    lab2 solution

---
+++ lab2/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Results(FormModel model)
+        {
+            int Year = model.Date.Year;
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
+                model.LeapYear = true;
+            else
+                model.LeapYear = false;
+
+            DateTime a = model.Date;
+            DateTime b = DateTime.Now;
+            TimeSpan duration = b - a;
+
+            model.DaysSinceBirth = duration.Days;
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ lab2/Models/FormModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab2.Models
+{
+    public class FormModel
+    {
+        public string Name { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]
+        public DateTime Date { get; set; }
+        public bool LeapYear { get; set; }
+        public int DaysSinceBirth { get; set; }
+    }
+}
